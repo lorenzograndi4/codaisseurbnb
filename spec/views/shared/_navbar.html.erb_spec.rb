195aff1f082ad54bb15507a31a8cb73989e12550
lorenzograndi4 <lorenzo.grandi4@gmail.com>
2017-07-05 13:30:23 +0200

    first test working

--- spec/views/shared/_navbar.html.erb_spec.rb
+++ spec/views/shared/_navbar.html.erb_spec.rb
@@ -1,9 +1,19 @@
 require 'rails_helper'
 
 describe "shared/_navbar.html.erb" do
-  before { sign_in user }
+  #  Write an extra test for the navbar that checks if the log in
+  #  and sign up links get rendered when the user is not signed in.
+
+  context "when user not signed in" do
+      it "shows login and signup links" do
+        render
+        expect(rendered).to have_content "Log In"
+        expect(rendered).to have_content "Sign Up"
+      end
+  end
 
   context "without profile" do
+    before { sign_in user }
     let(:user) { create :user }
 
     it "renders email" do
@@ -13,10 +23,11 @@ describe "shared/_navbar.html.erb" do
   end
 
   context "with profile" do
+    before { sign_in user }
     let(:profile) { build :profile }
     let(:user) { create :user, profile: profile }
 
-    it "renders first and last name" do
+    it "renders full name" do
       render
       expect(rendered).to have_content profile.first_name
       expect(rendered).to have_content profile.last_name
