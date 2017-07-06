19e66b402f03a2c022a458664b5e55998c1238e3
lorenzograndi4 <lorenzo.grandi4@gmail.com>
2017-07-06 08:56:57 +0200

    add nav tests

--- spec/features/navigate_events_spec.rb
+++ spec/features/navigate_events_spec.rb
@@ -1,7 +1,19 @@
 require 'rails_helper'
 
 describe "navigation between events" do
-  it "..." do
+  before {login_as user }
+  let(:user) { create :user}
+  let!(:event) { create :event, user: user }
 
+  it "allows navigation from show page to index page" do
+    visit event_url(event)
+    click_link "Back"
+    expect(current_path).to eq(events_path)
+  end
+
+  it "allows navigation from index page to show page" do
+    visit events_url
+    click_link "View"
+    expect(current_path).to eq(event_path(event))
   end
 end
