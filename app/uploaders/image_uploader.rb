87c3f40b4a114e70bf3d792396680dd218fc28bb
lorenzograndi4 <lorenzo.grandi4@gmail.com>
2017-07-06 17:58:39 +0200

    add tiny and thumbnail versions of images in all views

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,3 +1,16 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
+
+  version :thumbnail do
+    eager
+    resize_to_fit(300, 300)
+    cloudinary_transformation :quality => 80
+  end
+
+  version :tiny do
+    eager
+    resize_to_fit(100, 100)
+    cloudinary_transformation :quality => 80
+  end
+
 end
